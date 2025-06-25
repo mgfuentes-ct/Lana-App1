09fd9985db8b7f5805005872524ad88e868da07d
Maria <146016427+mgfuentes-ct@users.noreply.github.com>
2025-06-25 02:53:07 -0600

    elimine una vista innecesaria

--- Lana-App/App.js
+++ Lana-App/App.js
@@ -8,7 +8,6 @@ import RegisterScreen from './screens/RegisterScreen';
 import LoginScreen from './screens/LoginScreen';
 import NotificationCenterScreen from './screens/NotificationCenterScreen';
 import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
-import DashboardScreen from './screens/DashboardScreen';
 import TransactionFormScreen from './screens/TransactionFormScreen';
 import ChartsScreen from './screens/ChartsScreen';
 import TransactionDetailScreen from './screens/TransactionDetailScreen';
@@ -33,7 +32,6 @@ export default function App() {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Notificaciones" component={NotificationCenterScreen} />
         <Stack.Screen name="RecuperarContraseña" component={ForgotPasswordScreen} />
-        <Stack.Screen name="PanelPrincipal" component={DashboardScreen} />
         <Stack.Screen name="NuevaTransacción" component={TransactionFormScreen} />
         <Stack.Screen name="Gráficas" component={ChartsScreen} />
         <Stack.Screen name="DetalleTransacción" component={TransactionDetailScreen} />
